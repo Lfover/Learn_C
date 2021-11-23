90080e098f0a1d0470bb4a6ece0d0ab1eaab4ff9
LMR <3524613297@qq.com>
2021-11-23 22:53:10 +0800

    模拟实现strstr,和字母分析解法

---
+++ 21-11-23/21-11-23/analyse word.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <Windows.h>
+//#include <string.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	char str[1000];
+//	int num[1000];
+//	cin>>str;
+//	int len = strlen(str);
+//	for (int i = 0; i < len; i++){
+//		num[(int)str[i] - (int)'a']++;
+//	}
+//	for (int i = 0; i < 26; i++){
+//
+//	}
+//	cout << str;
+//	system("pause");
+//	return 0;
+//}
