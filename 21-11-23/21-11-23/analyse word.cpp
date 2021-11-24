3e961f0b38f577ff73f8d19fb69169eaf470e734
LMR <3524613297@qq.com>
2021-11-24 23:05:31 +0800

    增加了代词分析的另一种解法

--- 21-11-23/21-11-23/analyse word.cpp
+++ 21-11-23/21-11-23/analyse word.cpp
@@ -1,22 +1,25 @@
-//#include <iostream>
-//#include <Windows.h>
-//#include <string.h>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	char str[1000];
-//	int num[1000];
-//	cin>>str;
-//	int len = strlen(str);
-//	for (int i = 0; i < len; i++){
-//		num[(int)str[i] - (int)'a']++;
-//	}
-//	for (int i = 0; i < 26; i++){
-//
-//	}
-//	cout << str;
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+#include <windows.h>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+ //   unordered_map<int, int>frequency;
+	char str[1000];
+	int num[1000] = { 0 };
+	cin>>str;
+	int max = 0;
+	int len = strlen(str);
+	for (int i = 0; i < len; i++){
+		num[(int)str[i] - (int)'a']++;
+	}
+	for (int i = 0; i < 26; i++){
+		if (num[max] < num[i+1]){
+			max = i + 1;
+		}
+	}
+	cout << (char)(max + 97) << endl<<num[max]<<endl;
+	system("pause");
+	return 0;
+}
--- 21-11-23/21-11-23/strstr.cpp
+++ 21-11-23/21-11-23/strstr.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <Windows.h>
-#include <assert.h>
-using namespace std;
-
-const char *my_strstr(const char *str, const char *sub)
-{
-	assert(str);
-	assert(sub);
-	const char *str_start = str;
-	const char *str_p = str;
-	const char *sub_p = sub;
-
-	while (*str_start){
-		str_p = str_start;
-		sub_p = sub;
-		while (*sub_p&&*str_p&&*str_p==*sub_p){
-			str_p++;
-			sub_p++;
-		}
-		if (*sub_p== '\0'){
-			return str_start;
-		}
-		str_start++;
-	}
-	return NULL;
-}
-int main()
-{
-	const char *str = "hello 123 abc456xyz,12,xy1234";
-	const char *sub = "xyz";
-	cout << my_strstr(str, sub);
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <Windows.h>
+//#include <assert.h>
+//using namespace std;
+//
+//const char *my_strstr(const char *str, const char *sub)
+//{
+//	assert(str);
+//	assert(sub);
+//	const char *str_start = str;
+//	const char *str_p = str;
+//	const char *sub_p = sub;
+//
+//	while (*str_start){
+//		str_p = str_start;
+//		sub_p = sub;
+//		while (*sub_p&&*str_p&&*str_p==*sub_p){
+//			str_p++;
+//			sub_p++;
+//		}
+//		if (*sub_p== '\0'){
+//			return str_start;
+//		}
+//		str_start++;
+//	}
+//	return NULL;
+//}
+//int main()
+//{
+//	const char *str = "hello 123 abc456xyz,12,xy1234";
+//	const char *sub = "xyz";
+//	cout << my_strstr(str, sub);
+//	system("pause");
+//	return 0;
+//}
